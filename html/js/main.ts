99fa160e3c2bc8b7ada8fa42a1b4ad4f1aed3c3b
viktorian <iamviktorian@gmail.com>
2017-09-09 00:00:12 +0300

    Verstka and interfaces

---
+++ html/js/main.ts
@@ -0,0 +1,19 @@
+interface User {
+	
+	id: number;
+    name: string;
+	f_name: string;
+	password: any;
+	
+}
+
+interface Lesson {
+	
+	id: number;
+	name: string;
+	date: number;
+	desc: string | number;
+	src: string;
+	duration: number;
+	
+}
